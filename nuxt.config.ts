4e524e726cd60c8705713ba5a92787afb6daff79
Oussama BOUHLEL <oussama_bouhlel@outlook.fr>
2025-05-15 10:52:36 +0200

    Home page ok

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
 
+  css: ['~/assets/css/global.css'],
+
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/test-utils',
-    '@nuxt/scripts',
-    '@nuxt/ui',
-    '@nuxt/content'
-  ]
-})
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/test-utils",
+    "@nuxt/scripts",
+    "@nuxt/ui",
+  ],
+});
