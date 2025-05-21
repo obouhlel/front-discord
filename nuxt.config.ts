ed68f9ab42738d2b5817453af85d56abea1f8eeb
Oussama BOUHLEL <oussama_bouhlel@outlook.fr>
2025-05-21 16:06:26 +0200

    Add auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
+
+  runtimeConfig: {
+    public: {
+      BACK_URL: process.env.BACK_URL || "https://api.neko.oustopie.xyz",
+    }
+  },
+
+  devServer: {
+    port: 3333,
+  },
+
   devtools: { enabled: true },
 
-  css: ['~/assets/css/global.css'],
+  css: ["~/assets/css/global.css"],
 
   colorMode: {
-    preference: 'system',
-    fallback: 'light',
-    hid: 'nuxt-color-mode-script',
-    globalName: '__NUXT_COLOR_MODE__',
-    componentName: 'ColorScheme',
-    classPrefix: '',
-    classSuffix: '',
-    storage: 'localStorage',
-    storageKey: 'data-theme'
+    preference: "system",
+    fallback: "light",
+    hid: "nuxt-color-mode-script",
+    globalName: "__NUXT_COLOR_MODE__",
+    componentName: "ColorScheme",
+    classPrefix: "",
+    classSuffix: "",
+    storage: "localStorage",
+    storageKey: "data-theme",
   },
 
   modules: [
---
+++ types/user.type.ts
@@ -0,0 +1,9 @@
+export type User = {
+  name: string;
+  id: number;
+  username: string;
+  email: string;
+  discordId: string;
+  anilistId: string | null;
+  avatarId: string;
+};
